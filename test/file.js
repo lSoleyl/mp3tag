86d84ef21649f706ee7080322821bd2a0e3815aa
lSoleyl <lSoleyl+github@gmail.com>
2018-10-18 20:35:02 +0200

    Fixed file test by providing correct path and add .vscode to gitignore

--- test/file.js
+++ test/file.js
@@ -2,27 +2,28 @@
  */
 var File = require('../file')
 var fs = require('fs')
+var os = require('os')
+var path = require('path')
 var should = require('chai').should()
 
 
 describe('File', function() {
-  var path = '/tmp/writeTest'
+  
+  var filePath = path.join(os.tmpdir(), 'writeTest.txt');
 
   beforeEach(function() {
-    if (fs.existsSync(path))
-      fs.unlinkSync(path)
+    if (fs.existsSync(filePath))
+      fs.unlinkSync(filePath)
   })
-
-  //TODO determine temp directory based on running operating system /tmp vs %TEMP%
 
   describe('openFile(w)', function() {
     it('should create a new file if it doesn\'t exist', function(done) {
-      File.open(path, "w", function(err, file) {
+      File.open(filePath, "w", function(err, file) {
         if (err)
           return done(err)
 
         file.close()
-        fs.existsSync(path).should.equal(true)
+        fs.existsSync(filePath).should.equal(true)
         done()
       })
     })
@@ -33,14 +34,14 @@ describe('File', function() {
 
     it('should not clear the file on open', function(done) {
       //Create file with content in it
-      fs.writeFileSync(path, content)
+      fs.writeFileSync(filePath, content)
 
-      File.open(path, "a", function(err, file) {
+      File.open(filePath, "a", function(err, file) {
         if (err)
           return done(err)
 
         file.close()
-        fs.readFileSync(path).toString().should.equal(content)
+        fs.readFileSync(filePath).toString().should.equal(content)
         done() 
       })
     })
@@ -50,9 +51,9 @@ describe('File', function() {
       var result = "xxxx content"
 
       //Create file with content in it
-      fs.writeFileSync(path, content)
+      fs.writeFileSync(filePath, content)
 
-      File.open(path, "a", function(err, file) {
+      File.open(filePath, "a", function(err, file) {
         if (err)
           return done(err)
 
@@ -62,7 +63,7 @@ describe('File', function() {
 
           file.close()
 
-          fs.readFileSync(path).toString().should.equal(result)
+          fs.readFileSync(filePath).toString().should.equal(result)
           done()
         })
       })
@@ -73,7 +74,7 @@ describe('File', function() {
     var content = "Hello Buffers!"
 
     it("should start writing at the file's beginning", function(done) {
-      File.open(path, "w", function(err, file) {
+      File.open(filePath, "w", function(err, file) {
         if (err)
           return done(err)
 
@@ -86,7 +87,7 @@ describe('File', function() {
 
           file.close()
           bytes.should.equal(buf.length)
-          fs.readFileSync(path).toString().should.equal(content)
+          fs.readFileSync(filePath).toString().should.equal(content)
           done()
         })
       })
@@ -95,7 +96,7 @@ describe('File', function() {
     it("should continue writing where it left off, when writing multiple times", function(done) {
       var content = [ "This is the first part\n", "This is the last part" ] 
 
-      File.open(path, "w", function(err, file) {
+      File.open(filePath, "w", function(err, file) {
         if (err)
           return done(err)
 
@@ -112,7 +113,7 @@ describe('File', function() {
 
             file.close()
 
-            fs.readFileSync(path).toString().should.equal(content.join(""))
+            fs.readFileSync(filePath).toString().should.equal(content.join(""))
             done()
           })
         })
